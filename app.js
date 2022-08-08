d76dbcd128e032bece8be953f937dc2322b41e4a
sebastiancifuentes28 <s.cifu28@gmail.com>
2022-08-08 18:50:11 -0500

    Agregar productDetails.html y stylesProduct.css

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -9,18 +10,16 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'))
-})
-app.get('/', (req, res) => {
+});
+app.get('/login.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'))
-})
-app.get('/', (req, res) => {
+});
+app.get('/productCart.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/productCart.html'))
-})
-app.get('/', (req, res) => {
+});
+app.get('/productDetail.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
-})
-app.get('/', (req, res) => {
+});
+app.get('/register.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
-})
-
-app.use(express.static('public'));
+});
